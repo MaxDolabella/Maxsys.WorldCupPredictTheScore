062b082b2cf02e8f1d8fd1573de1af09d760cc30
Max Dolabella <max.dolabella@headsoft.com.br>
2022-12-03 16:50:45 -0300

    Adicionado RepositoryBase

---
+++ src/Maxsys.WorldCupPredictTheScore.Web/Core/Repositories/Common/RepositoryBase'T.cs
@@ -0,0 +1,15 @@
+﻿using Maxsys.WorldCupPredictTheScore.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Maxsys.WorldCupPredictTheScore.Web.Core.Repositories.Common;
+
+public abstract class RepositoryBase<T> : RepositoryBase where T : class
+{
+    protected readonly DbSet<T> _dbSet;
+
+    public RepositoryBase(ApplicationDbContext context)
+        : base(context)
+    {
+        _dbSet = context.Set<T>();
+    }
+}
---
+++ src/Maxsys.WorldCupPredictTheScore.Web/Core/Repositories/Common/RepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using Maxsys.WorldCupPredictTheScore.Web.Data;
+
+namespace Maxsys.WorldCupPredictTheScore.Web.Core.Repositories.Common;
+
+public abstract class RepositoryBase
+{
+    protected readonly ApplicationDbContext _context;
+
+    public RepositoryBase(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+}
